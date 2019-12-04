4b9e2520f31b539c11d0ad1c51c0e6c2373f6bd1
Pedro Piñera <pedro@ppinera.es>
2019-12-04 08:57:27 +0100

    Generate documentation page

--- website/src/templates/documentation.jsx
+++ website/src/templates/documentation.jsx
@@ -2,9 +2,29 @@
 import { jsx } from 'theme-ui'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
+import 'semantic-ui-css/semantic.min.css'
 
-const DocumentationPage = () => {
-  return <Layout>Documentation</Layout>
+const DocumentationPage = ({ data: { mdx } }) => {
+  const post = mdx
+  return (
+    <Layout>
+      <MDXRenderer>{post.body}</MDXRenderer>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        siteUrl
+      }
+    }
+    mdx(fields: { slug: { eq: $slug } }) {
+      body
+    }
+  }
+`
 
 export default DocumentationPage
